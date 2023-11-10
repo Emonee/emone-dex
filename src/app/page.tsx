26e872e188c59386ccebc8e5c147d37ac92cf2e2
Emiliano <e.acevedo.felip@gmail.com>
2023-11-10 10:04:24 -0300

    sticky header with search input in root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,25 +9,29 @@ const pokemons = pokemonJsonData.results
 export default function Home () {
   const [searchInpValue, setSearchInpValue] = useState('')
   return (
-    <main className='p-5'>
-      <input
-        type='text'
-        value={searchInpValue}
-        onChange={(e) => setSearchInpValue(e.target.value)}
-        placeholder='Search a Pokemon (Gen III)'
-        className='block mx-auto mb-4 text-slate-800 rounded-lg p-2 text-xl'
-      />
-      <section className='flex flex-wrap justify-center items-center gap-5'>
-        {pokemons.map(({ name }) => (
-          <Link
-            key={name}
-            href={name}
-            className={`block w-32 p-2 border rounded-lg text-center capitalize bg-slate-800 hover:bg-slate-700 transition-[background-color] ${!name.toLowerCase().includes(searchInpValue.toLowerCase()) ? 'hidden' : ''}`}
-          >
-            {name}
-          </Link>
-        ))}
-      </section>
-    </main>
+    <>
+      <header className='backdrop-blur-sm sticky top-0 py-6 shadow-2xl'>
+        <input
+          type='text'
+          value={searchInpValue}
+          onChange={(e) => setSearchInpValue(e.target.value)}
+          placeholder='Search a Pokemon (Gen III)'
+          className='block mx-auto text-slate-800 rounded-lg p-2 text-xl'
+        />
+      </header>
+      <main className='px-5 py-10'>
+        <section className='flex flex-wrap justify-center items-center gap-5'>
+          {pokemons.map(({ name }) => (
+            <Link
+              key={name}
+              href={name}
+              className={`block w-32 p-2 border rounded-lg text-center capitalize bg-slate-800 hover:bg-slate-700 transition-[background-color] ${!name.toLowerCase().includes(searchInpValue.toLowerCase()) ? 'hidden' : ''}`}
+            >
+              {name}
+            </Link>
+          ))}
+        </section>
+      </main>
+    </>
   )
 }
