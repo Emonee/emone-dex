6e4b0b731d385c7901e5167fb93f3ef5379e8039
Emiliano <e.acevedo.felip@gmail.com>
2023-11-10 10:28:20 -0300

    new header styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,13 +19,13 @@ export default function Home () {
 
   return (
     <>
-      <header className='backdrop-blur-sm sticky top-0 py-6 shadow-2xl'>
+      <header className='backdrop-blur-sm sticky top-0 py-6 px-5 shadow-2xl backdrop-brightness-50'>
         <input
           type='text'
           defaultValue={searchParams.get('search')?.toString()}
           onChange={(e) => setQueryParamSearch(e.target.value)}
           placeholder='Search a Pokemon (Gen III)'
-          className='block mx-auto text-slate-800 rounded-lg p-2 text-xl'
+          className='block mx-auto text-slate-800 rounded-lg p-2 text-xl max-w-full'
         />
       </header>
       <main className='px-5 py-10'>
