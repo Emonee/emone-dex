04aff5bad156687d0c5f83df56fa966acc734b97
Emiliano <e.acevedo.felip@gmail.com>
2023-11-15 00:22:52 -0300

    fix: add /emone-dex in PkmSmallLinkCard images src for production

--- src/components/cards/PkmSmallLinkCard.tsx
+++ src/components/cards/PkmSmallLinkCard.tsx
@@ -3,6 +3,8 @@ import { type Pokemon } from '@/types/types'
 import Image from 'next/image'
 import Link from 'next/link'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 export default function PkmSmallLinkCard ({ pkm }: { pkm: Pokemon }) {
   const typeNames = pkm.types.map(({ type: { name } }) => sanitizeType(name)) as [string] | [string, string]
 
@@ -12,7 +14,7 @@ export default function PkmSmallLinkCard ({ pkm }: { pkm: Pokemon }) {
       <p className='text-xl capitalize'>{pkm.name}</p>
       <div className='flex flex-col justify-center items-center ml-auto gap-3'>
         {typeNames[0] === typeNames[1]
-          ? <Image src={`/images/type_icons/${typeNames[0]}_icon.png`} height={20} width={20} alt={`${typeNames[0]} icon`} />
+          ? <Image src={`${(isProduction && '/emone-dex') || ''}/images/type_icons/${typeNames[0]}_icon.png`} height={20} width={20} alt={`${typeNames[0]} icon`} />
           : typeNames.map(name => (
             <Image key={name} src={`/images/type_icons/${name}_icon.png`} height={20} width={20} alt={`${name} icon`} />
           ))}
